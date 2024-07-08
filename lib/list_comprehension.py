4cec8f602fd6099daf6675bc0046f4acbf472085
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-07-08 12:51:03 +0300

    Completes Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,19 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even = []
+    for i in num_list:
+        if i % 2 == 0:
+            even.append(i)
+    return even
+
+print(return_evens([2,5,7,8,10]))
 
 def make_exclamation(sentence_list):
-    pass
+    new = []
+    for i in sentence_list:
+        i =  i +"!"
+        new.append(i)
+    return new
+
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
